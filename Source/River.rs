b22475d4ac47af4d6ad586c067a7442d7881d426
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-06 22:58:32 +0300

    squash!

--- Source/River.rs
+++ Source/River.rs
@@ -1,9 +1,6 @@
 use Echo::Fn::Job::{Action, ActionResult, Work, Worker};
 
-use futures::future::join_all;
 use std::sync::Arc;
-use tokio::{net::TcpListener, sync::mpsc};
-use tokio_tungstenite::accept_async;
 
 struct Site;
 
@@ -25,22 +22,25 @@ impl Worker for Site {
 #[tokio::main]
 async fn main() {
 	let Work = Arc::new(Work::Begin());
-	let (Approval, Receipt) = mpsc::unbounded_channel();
+	let (Approval, Receipt) = tokio::sync::mpsc::unbounded_channel();
 
 	// @TODO: Auto-calc number of workers on the force
 	let Force: Vec<_> = (0..4)
 		.map(|_| tokio::spawn(Echo::Fn::Job::Fn(Arc::new(Site), Work.clone(), Approval.clone())))
 		.collect();
 
-	while let Ok((stream, _)) =
-		TcpListener::bind("127.0.0.1:9998").await.expect("Cannot TcpListener.").accept().await
+	while let Ok((stream, _)) = tokio::net::TcpListener::bind("127.0.0.1:9998")
+		.await
+		.expect("Cannot TcpListener.")
+		.accept()
+		.await
 	{
 		tokio::spawn(Echo::Fn::Job::Yell::Fn(
-			accept_async(stream).await.expect("Cannot accept_async."),
+			tokio_tungstenite::accept_async(stream).await.expect("Cannot accept_async."),
 			Work.clone(),
 			Receipt,
 		));
 	}
 
-	join_all(Force).await;
+	futures::future::join_all(Force).await;
 }
