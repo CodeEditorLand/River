87d2cdde156c917fb3eda4e21f1acb659dbeb2f6
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-07 12:13:27 +0300

    squash!
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- Source/River.rs
+++ Source/River.rs
@@ -6,8 +6,26 @@ use std::sync::Arc;
 
 struct Site;
 
+/// Implements the `Worker` trait for the `Site` struct, allowing it to process actions.
+///
+/// This implementation handles the `Read` action by attempting to read the content of the specified file path asynchronously.
+/// If successful, it returns an `ActionResult` with the content. If an error occurs, it returns an `ActionResult` with an error message.
+/// For any other action, it returns a generic error message indicating that the action cannot be processed.
+///
 #[async_trait::async_trait]
 impl Worker for Site {
+	/// # Arguments
+	///
+	/// * `Action` - The action to be processed. This can be a `Read` action with a specified file path.
+	///
+	/// # Returns
+	///
+	/// An `ActionResult` containing the result of the action. If the action is a `Read` action and the file is read successfully,
+	/// the result will contain the file content. If an error occurs during the read operation, the result will contain an error message.
+	/// For any other action, the result will contain a generic error message.
+	///
+	/// This implementation ensures that the `Site` struct can handle `Read` actions by reading the file content asynchronously and returning the result.
+	/// For any other actions, it returns a generic error message indicating that the action cannot be processed.
 	async fn Receive(&self, Action: Action) -> ActionResult {
 		match Action {
 			Action::Read { ref Path } => match tokio::fs::read_to_string(&Path).await {
@@ -21,6 +39,32 @@ impl Worker for Site {
 	}
 }
 
+/// The main entry point of the application.
+///
+/// This function initializes the work queue, sets up a channel for approval messages,
+/// spawns worker tasks to process actions, and listens for incoming TCP connections
+/// to handle WebSocket communication.
+///
+/// # Behavior
+///
+/// 1. Initializes the work queue.
+/// 2. Creates an unbounded channel for approval messages.
+/// 3. Spawns worker tasks to process actions from the work queue.
+/// 4. Binds to a TCP listener and accepts incoming connections.
+/// 5. For each accepted connection, spawns a task to handle the WebSocket connection.
+/// 6. Waits for all worker tasks to complete.
+///
+/// # Panics
+///
+/// This function will panic if:
+/// - The TCP listener cannot be bound to the specified address.
+/// - A WebSocket connection cannot be accepted.
+///
+/// # TODO
+///
+/// - Auto-calculate the number of workers.
+///
+/// ```
 #[tokio::main]
 async fn main() {
 	let Work = Arc::new(Work::Begin());
